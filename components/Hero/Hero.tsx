8e578dee7ea429b819d049e4de678b567e55ddd0
Pasindu P Konghawaththa <pasindudixon25@gmail.com>
2021-07-16 20:41:34 +0530

    add terminal small cards

--- components/Hero/Hero.tsx
+++ components/Hero/Hero.tsx
@@ -14,11 +14,27 @@ const Hero = () => {
     >
       <Grid templateColumns="repeat(2, 1fr)" gap={6}>
         <Box w="100%" h="10">
-          <Text color="#1390EB" fontWeight="bold">
+          <Text
+            color="#1390EB"
+            fontWeight="bold"
+            animation="moveInRight  ease-out 1s"
+          >
             Looking for a Developer ?
           </Text>
-          <Text fontSize="6xl" color="#ffffff" fontFamily="Montserrat">
+          <Text
+            fontSize="6xl"
+            color="#ffffff"
+            fontFamily="Montserrat"
+            animation="moveInLeft ease-out 1s"
+          >
             Web Application Developer and Designer
+          </Text>
+          <Text
+            fontSize="2xl"
+            color="#ffffff"
+            animation="moveInButton ease-in 1s"
+          >
+            A Designer That Improve The Quality Of Life Of Millions Of People.
           </Text>
         </Box>
         <Box w="100%" h="10">
