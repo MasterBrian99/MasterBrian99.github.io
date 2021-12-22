64f90d9e421630ac15723ba495c544b0e6f15d5a
Pasindu P Konghawaththa <pasindudixon25@gmail.com>
2021-12-22 21:20:34 +0530

    remove old

--- next.config.js
+++
@@ -1,7 +0,0 @@
-
-const isProd = process.env.NODE_ENV === 'production'
-
-module.exports = {
-  // Use the CDN in production and localhost for development.
-  assetPrefix: isProd ? 'https://cdn.statically.io/gh/masterbrian99/masterbrian99.github.io/gh-pages/' : '',
-}
--- types/index.d.ts
+++
@@ -1,5 +0,0 @@
-declare module "*.png";
-declare module "*.jpg";
-declare module "*.jpeg";
-declare module "*.mov";
-declare module "react-typical";
