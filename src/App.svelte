3b61c90c9a34324947119b9d58f342245c515ac1
pasindu p konghawaththa <37585474+MasterBrian99@users.noreply.github.com>
2022-09-14 18:40:25 +0530

    add routing

--- src/App.svelte
+++ src/App.svelte
@@ -1,13 +1,21 @@
 <script lang="ts">
-
+  import {Router, Link, Route} from 'svelte-navigator';
+  import Home from './pages/Home.svelte';
+  import Main from './pages/Main.svelte';
 </script>
 
-<main>
-  <h1 class="text-3xl font-bold underline h1-header">Hello world!</h1>
-</main>
+<Router>
+  <nav>
+    <Link to="/">Home</Link>
+    <Link to="/main">About</Link>
+  </nav>
+  <div>
+    <Route path="/">
+      <Home />
+    </Route>
+    <Route path="/main">
+      <Main />
+    </Route>
+  </div>
+</Router>
 
-<style lang="scss">
-  h1 {
-    background-color: green;
-  }
-</style>
