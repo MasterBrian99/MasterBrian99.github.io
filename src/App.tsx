37a154e99317e5ed062a17cc1d8d985c4343dba9
Pasindu P Konghawaththa <pasindudixon25@gmail.com>
2022-12-17 20:18:19 +0530

    add dark theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,17 @@
 import { useState } from "react";
+import ThemeSwitch from "./components/ThemeSwitch/ThemeSwitch";
+import ThemeContextWrapper from "./context/ThemeContext/ThemeContextWrapper";
 
 function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <>
+      <ThemeContextWrapper>
+        <ThemeSwitch />
+        <h1 className="h-screen w-screen min-h-screen bg-neutral-900 dark:bg-green-500">
+          Hello world!
+        </h1>
+      </ThemeContextWrapper>
+    </>
+  );
 }
-
 export default App;
