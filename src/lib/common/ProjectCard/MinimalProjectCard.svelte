ff6e6409d3647a7faa2b4846e2a7cb1c88cdc5c9
Pasindu P Konghawaththa <pasindudixon25@gmail.com>
2023-03-12 23:33:38 +0530

    remove old files

--- src/lib/common/ProjectCard/MinimalProjectCard.svelte
+++
@@ -1,8 +0,0 @@
-<script lang="ts">
-</script>
-
-<div class="w-84 h-40  bg-neutral-900 rounded-md p-3 relative">
-	<h2>Creative Title</h2>
-	<p class="text-sm my-2">asdasdad</p>
-	<div class="absolute bottom-0 right-0 m-3">asd</div>
-</div>
--- src/lib/components/About/AboutSkill.svelte
+++
@@ -1,7 +0,0 @@
-<script lang="ts">
-	import HeaderText from '$lib/common/HeaderText/HeaderText.svelte';
-</script>
-
-<section class="w-full max-h-full bg-black pb-12 min-h-screen text-white">
-	<HeaderText title="What I Do" />
-</section>
