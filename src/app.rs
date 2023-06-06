58bea857c940dcd0807d11c8761c62f3d3930f73
Pasindu Pramodya <pasindudixon25@gmail.com>
2023-06-06 23:28:13 +0530

    add tailwind

--- src/app.rs
+++ src/app.rs
@@ -4,9 +4,8 @@ use yew::prelude::*;
 pub fn app() -> Html {
     html! {
         <main>
-            <img class="logo" src="https://yew.rs/img/logo.png" alt="Yew logo" />
-            <h1>{ "Hello World!" }</h1>
-            <span class="subtitle">{ "from Yew with " }<i class="heart" /></span>
+      
+            <h1  class="text-green-300">{"Hello"} </h1>
         </main>
     }
 }
