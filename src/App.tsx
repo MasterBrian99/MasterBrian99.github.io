532b35e5ec0ad834b68b6354c28a54fe29402a08
Pasindu Pramodya <pasindudixon25@gmail.com>
2023-06-08 20:08:30 +0530

    add react and tailwind

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+
+function App() {
+
+  return (
+    <>
+    <section className="">
+    <h1>Hello</h1>
+    <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+    </section>
+    </>
+  )
+}
+
+export default App
