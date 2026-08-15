7b207e345102a1a5da2ebfbdbf9303b855f3ff29
Pasindu Pramodya <git@pasindupramodya.com>
2026-08-15 19:11:20 +0530

    feat: initialize project

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,33 +1,5 @@
 // @ts-check
-import { defineConfig } from "astro/config";
-
-import react from "@astrojs/react";
-import tailwindcss from "@tailwindcss/vite";
-import sitemap from "@astrojs/sitemap";
-
+import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({
-  site: "https://pasindupramodya.com",
-  integrations: [
-    react(),
-    sitemap({
-      customPages: ["https://pasindupramodya.com/"],
-      serialize(item) {
-        return {
-          ...item,
-          changefreq: /** @type {import('@astrojs/sitemap').ChangeFreqEnum} */ (
-            "weekly"
-          ),
-          priority: item.url === "https://pasindupramodya.com/" ? 1.0 : 0.8,
-          lastmod: new Date().toISOString(),
-        };
-      },
-    }),
-
-  ],
-
-  vite: {
-    plugins: [tailwindcss()],
-  },
-});
+export default defineConfig({});
